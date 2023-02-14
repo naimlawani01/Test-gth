6098407ae9cd0c977a7aa43cca9429e6a15d3f7b
Imrane Lawani <naimlawani01@gmail.com>
2023-02-14 12:51:38 +0100

    HEADER BIS

--- views/index.php
+++ views/index.php
@@ -11,15 +11,24 @@ require('../app/function.php');
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+            <!-- Bootstrap CSS -->
     </head>
-    <body>
+    <body >
         <?php require_once('header.php'); ?>
+        <div class="container">
+            <p>Home > <a href="">Posts</a> </p>
+            <div class="title">
+                <h1>Nos meilleurs Posts</h1>
+            </div>
+        </div>
+
         <div class="container" style="margin-top: 20px;">
             <?php foreach($posts as $post):?>
-                <div class="card" style="width: 50rem;">
+                <div class="card" >
                     <div class="card-body">
                         <h4 class="card-title"><?=$post['title'] ?></h4><?=$post['post_date'] ?>
                         <?php $user= getUser($post['user_id'], $pdo); ?>
@@ -38,6 +47,8 @@ require('../app/function.php');
                 <?php endif; ?>
         </div>
         <!-- Optional JavaScript -->
+        <!--js script-->
+        <script type="text/javascript" src="script.js"></script>
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
