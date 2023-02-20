6c4fc70b2d4c40109927901240d66c87fdc914d5
Imrane Lawani <naimlawani01@gmail.com>
2023-02-20 20:37:26 +0100

    Prod

--- app/dbconnexion.php
+++ app/dbconnexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost' ;
-$dbname= 'naimimranelawani';
-$dbuser= 'root';
-$dbpwd= '';
+$host = 'eu-cdbr-west-03.cleardb.net' ;
+$dbname= 'heroku_f9faca63e83c36f';
+$dbuser= 'bda5240d70b1f3';
+$dbpwd= '8957a2ce';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 try {
     $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname, $dbuser, $dbpwd , $options);
